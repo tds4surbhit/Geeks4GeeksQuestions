c0fe35e0f2108f252936fe75ca9d6d151bdd6674
Surbhit Sinha <surbhitsinha@Surbhits-MacBook-Air.local>
2024-01-21 00:28:35 +0530

    Adding HoareExampleQuickSort

--- src/Sorting/QuickSortWithLomutoPartition.java
+++ src/Sorting/QuickSortWithLomutoPartition.java
@@ -2,6 +2,25 @@ package Sorting;
 
 public class QuickSortWithLomutoPartition {
 
+    public static void swapMethod(int[] arr, int i , int j){
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+
+    public static int partitionLogic(int[] arr, int l, int h){
+        int i = l-1;
+        int pivot = arr[h];
+        for(int j = l; j < h ; j++){
+            if(arr[j] <= pivot){
+                i++;
+                swapMethod(arr, i, j);
+            }
+        }
+        swapMethod(arr, i + 1 , h);
+        return i+1;
+    }
+
     public static void printArray(int[] arr) {
         for(int element: arr) {
             System.out.print(element + " ");
@@ -9,16 +28,19 @@ public class QuickSortWithLomutoPartition {
         System.out.println();
     }
 
-    public static void QuickSortWithLomutoPartition(int[] arr){
-        int i = 0;
+    public static void QuickSortWithLomutoPartition(int[] arr, int l, int h){
+        if(h>l){
+            int partitionElement = partitionLogic(arr,l,h);
+            QuickSortWithLomutoPartition(arr,l,partitionElement-1);
+            QuickSortWithLomutoPartition(arr,partitionElement+1,h);
+        }
     }
 
     public static void main(String[] args){
         int[] arr = {8,4,7,9,3,10,5};
         int l = 0;
         int h = arr.length - 1;
-        QuickSortWithLomutoPartition(arr);
+        QuickSortWithLomutoPartition(arr,l,h);
         printArray(arr);
-
     }
 }
