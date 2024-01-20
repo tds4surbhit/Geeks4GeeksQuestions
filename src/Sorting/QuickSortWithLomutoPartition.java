2a08b0380d920911cc45a0925d0ad9784918e99e
Surbhit Sinha <surbhitsinha@Surbhits-MacBook-Air.local>
2024-01-20 22:35:24 +0530

    Adding something

--- src/Sorting/QuickSortWithLomutoPartition.java
+++ src/Sorting/QuickSortWithLomutoPartition.java
@@ -2,7 +2,23 @@ package Sorting;
 
 public class QuickSortWithLomutoPartition {
 
+    public static void printArray(int[] arr) {
+        for(int element: arr) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+
+    public static void QuickSortWithLomutoPartition(int[] arr){
+        int i = 0;
+    }
+
     public static void main(String[] args){
         int[] arr = {8,4,7,9,3,10,5};
+        int l = 0;
+        int h = arr.length - 1;
+        QuickSortWithLomutoPartition(arr);
+        printArray(arr);
+
     }
 }
